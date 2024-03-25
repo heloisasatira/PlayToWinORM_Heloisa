1d5a47a8218fc2517de35a09d73e546496bd42cc
Heloisa S. Rios <heloisasatirariosdossantos@gmail.com>
2024-03-25 17:25:52 -0300

    Criação do Model Usuario e execução do 1º sync.

--- db/index.js
+++ db/index.js
@@ -1,8 +1,19 @@
 require("dotenv").config();
 const connection = require("./db/connection");
 
-connection.authenticate().then(()=>{
+const Usuario = require("./models/Usuario");
+
+connection.
+sync()
+.then(() => {
+    console.log("Conectando e sincronizando!");
+})
+.catch((err) => {
+    console.log("Ocorreu um erro: " + err);
+});
+
+/* connection.authenticate().then(()=>{
     console.log("Conectado ao banco de dados com sucesso!");
 }).catch((err) =>{
     console.log("Error: " + err)
-});
+}); */
