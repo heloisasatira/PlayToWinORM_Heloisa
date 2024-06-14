ca96738191b6ba318ddb6826775ef349f1846330
Heloisa S. Rios <heloisasatirariosdossantos@gmail.com>
2024-06-14 16:11:24 -0300

    Nova rota de excluir criada para permitir a deleção dos usuários cadastrados.  Além disso, um formulário, na página usuarios.handlebars foi criado para permitir o processo de exclusão.

--- index.js
+++ index.js
@@ -55,12 +55,38 @@ app.post("/usuarios/novo", async (req, res) => {
     res.send("Usuário inserido sob o id " + usuario.id);
 });
 
-app.get("/usuarios/:id/atualizar", (req, res)=>{
+app.get("/usuarios/:id/atualizar", async (req, res)=>{
     const id = req.params.id;
-    const usuario = Usuario.findByPk(id, {raw: true});
-    
+    const usuario = await Usuario.findByPk(id, {raw: true});
     res.render("formUsuario", { usuario });
 });
+
+app.post("/usuarios/:id/atualizar", async (req, res)=>{
+    const id = req.params.id;
+
+    const dadosUsuario={
+    nickname : req.body.nickname,
+    nome: req.body.nome,
+    };
+    const registrosAfetados = await Usuario.update(dadosUsuario, {where: {id: id}});
+    if(registrosAfetados > 0){
+        res.redirect("/usuarios");
+    } else{
+        res.send("Erro ao atualizar usuário!");
+    }
+
+});
+
+app.post("/usuarios/excluir", async (req, res) =>{
+    const id = req.body.id;
+// tipo um delete
+    const registrosAfetados = await Usuario.destroy({where: {id: id}});
+    if(registrosAfetados > 0){
+        res.redirect("/usuarios");
+    } else{
+        res.send("Erro ao excluir usuário!");
+    }
+})
 
 app.listen(8000, () => {
     console.log("Server rodando na porta 8000¹");
